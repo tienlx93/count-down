93fc0029b11bad868f695d1fe2b6c524726ac6b0
Le Xuan Tien <tienle.dalat@gmail.com>
2018-09-07 22:09:57 +0700

    update background

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -3,8 +3,8 @@
 
   // Vide - Video Background Settings
   $('body').vide({
-    mp4: "mp4/linhlinh.mp4",
-    poster: "img/bg-mobile-fallback.jpg"
+    //mp4: "mp4/linhlinh.mp4",
+    poster: "https://travel.com.vn/images/destination/Large/dg_160519_du-lich-da-nang.jpg"
   }, {
     posterType: 'jpg'
   });
